fe5eee3c7872b84a0b79d83f06e1b873c86795ac
Damian <gladek123x@gmail.com>
2022-10-11 00:45:27 +0200

    end of general sorted files program

--- general.py
+++ general.py
@@ -21,4 +21,17 @@ def append_to_file(path, data):
     with open(path, 'a') as file:
         file.write(data + '\n')
 
-def delete_file_contents(path)
+def delete_file_contents(path):
+    with open(path, 'w'):
+        pass
+
+def file_to_set(file_name):
+    results = set()
+    with open(file_name, 'rt') as f:
+        for line in f:
+            return results.add(line.replace('\n', ''))
+
+def set_to_file(links, file):
+    delete_file_contents(file)
+    for link in sorted(links):
+        append_to_file(file,link)
