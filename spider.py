8c29b5c3b3136d70364134fb18ea1db9639c10e4
Damian <gladek123x@gmail.com>
2022-10-11 01:26:36 +0200

    check isues

--- spider.py
+++ spider.py
@@ -1,5 +1,5 @@
 from urllib.request import urlopen
-from link_finder import LinkFinder
+from link_finder import LinkParser
 from general import *
 
 
@@ -14,4 +14,11 @@ class Spider:
     queue = set()
     crawled = set()
 
-    # def __init__(self):
+    def __init__(self, project_name, base_url, domain_name):
+        Spider.project_name = project_name
+        Spider.base_url = base_url
+        Spider.domain_name = domain_name
+        Spider.queue_file = Spider.project_name + '/queue.txt'
+        Spider.crawled_file = Spider.project_name + '/crawled.txt'
+        self.boot()
+        self.crawl_page('Spider', Spider.base_url)
