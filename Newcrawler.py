6e24fca03db4fd1429a80f8f233eb6cdfc48eb9d
Damian <gladek123x@gmail.com>
2022-10-13 05:52:57 +0200

    finish

--- Newcrawler.py
+++ Newcrawler.py
@@ -4,6 +4,17 @@ import csv
 import pandas as pd
 
 
+def create_csv_headers():
+    headers = ["link",
+               "title",
+               "number of internal links",
+               "number of external links",
+               "number of times url was referenced by other pages"]
+    with open("data.csv", "w") as file:
+        create = csv.writer(file)
+        create.writerow(headers)
+
+
 class WebCrawler:
 
     def __init__(self, URL):
@@ -25,12 +36,11 @@ class WebCrawler:
         self.internal_numbers = None
         self.link_title_dict = dict()
         self.id_link = dict()
-        self.create_csv_headers()
+        create_csv_headers()
         self.get_links()
 
-
     def get_links(self):
-        self.pages_idx = 0
+        self.pages_idx = -1
         self.url = self.URL
         self.base_url = self.url
         self.response = requests.get(self.url)
@@ -55,11 +65,10 @@ class WebCrawler:
                 self.link_title_dict[get_link] = get_title
                 self.link_views_dict[get_link] = 1
                 self.id_link[self.IDX] = get_link
-                self.pages_idx += 1  # liczba podstron w danym linku
+                self.pages_idx += 1
                 self.IDX += 1
-        for k, v in self.link_title_dict.items():
-            print("{}: {}".format(k, v))
 
+        self.add_data_to_csv()
         self.new_url()
 
     def new_url(self):
@@ -104,38 +113,29 @@ class WebCrawler:
         if self.URL != self.base_url:
             self.external_numbers += 1
 
-
-    def create_csv_headers(self):
-        headers = ["link",
-                   "title",
-                   "number of internal links",
-                   "number of external links",
-                   "number of times url was referenced by other pages"]
-        with open("data.csv", "w") as file:
-            create = csv.writer(file)
-            create.writerow(headers)
-
     def add_data_to_csv(self):
         for k, v in self.link_title_dict.items():
+            print("{}: {}".format(k, v))
             data = {
-                'link' : [k],
-                'title' : [v],
-                'number of internal links': [],
-                'number of external links': [],
-                'number of times url was referenced by other pages':[]
+                'link': [k],
+                'title': [v],
+                'number of internal links': [self.pages_idx],
+                'number of external links': [0],
+                'number of times url was referenced by other pages': [0]
             }
             df = pd.DataFrame(data)
-            df.to_csv('data.csv', mode='a', index = False, header = False)
-
-
-
-
-
-
-
-
-
-
+            df.to_csv('data.csv', mode='a', index=False, header=False)
+            self.pages_idx = 0
 
 
 start = WebCrawler("https://www.youtube.com/")
+
+#TODO:Przepraszam za niekompletną funkcję zliczania
+# podstron i podstron podstron ale przez te nocne kodowanie przysypiam na fotelu ,
+# za mało czasu miałem na napisanie tego "Simple web crawler" z podkreśleniem na Simple :)
+# 4 dni walczyłem z szukaniem normal.mod na partycjach zeby zainstalować Ubuntu na tym dinozaurze (laptopie)
+# Cud że wogóle pyCharm na tym odpalił więc utknięcie w pętli podczas testowania programu kończyło się :black screen,
+# Coś w stylu : kiedy junior programuje ,google, stackoverflow, copy , paste , error : black screen , reset system , polubiłem już te memy :)
+# jeśli jakimś cudem dostanę się na ten staż to chyba tylko dlatego że ktoś doceni te moje wypociny i to że starałem
+# się jak tylko umiałem , niestety za mało czasu miałem żeby doprowadzić ten kod do 100% sprawności
+# dziękuje za poświęcony czas !
