587cfaed210ab17db3fa7a246c4e23a8248d86f4
Damian <gladek123x@gmail.com>
2022-10-15 00:21:04 +0200

    IMPLEMENTATION NEW ALGORITM 3/4

--- Newcrawler.py
+++ Newcrawler.py
@@ -9,7 +9,7 @@ def create_csv_headers():
                "title",
                "number of internal links",
                "number of external links",
-               "number of times url was referenced by other pages"]
+               "reference count"]
     with open("data.csv", "w") as file:
         create = csv.writer(file)
         create.writerow(headers)
@@ -17,165 +17,142 @@ def create_csv_headers():
 
 class WebCrawler:
 
-    def __init__(self, URL):
-        self.links_index_of_internal_links = dict() #słownik link:liczba podstron
-        self.link_title_dict = dict()               #słownik link:tytuł
-        self.id_link = dict()                       #słownik numer strony : tytuł
-        self.id_link[1] = URL
-        self.link_true_false = dict()               #słownik link: true/false
-        self.link_true_false[URL] = False
-        self.internallink_link = dict()             #słownik : podstrona : link
-        self.link_number_of_external_links = dict() #słownik link : liczba podstron
+    def __init__(self, URL, count_of_results):
+        self.links_internal_links_count_dict = dict()        # słownik link:liczba podstron
+        self.link_title_dict = dict()                        # słownik link:tytuł
+        self.id_link_dict = dict()                           # słownik numer strony : tytuł
+        self.id_link_dict[1] = URL
+        self.link_true_false_dict = dict()                   # słownik link: true/false
+        self.link_true_false_dict[URL] = False
+        self.internal_link_link_dict = dict()                # słownik : podstrona : link
+        self.link_number_of_external_links_dict = dict()     # słownik link : liczba podstron
+        self.clear_link_reference_count_dict = dict()        # słownik czysty link : liczba odnośników
+        self.link_reference_count_dict = dict()              # słownik link : liczba odnośników
         self.index_of_internal_external_links = 0
         self.search_link = URL
-        self.links_to_csv = dict()
-        self.new_url_for_spider = None
+        self.counter = count_of_results
         self.searchIDX = 0
-        self.actually_url = None
-        self.basic_url = None
-        self.base_url = None
-        self.external_numbers = 0
-        self.number_of_external_links = 0
+        self.true_false = None
         self.index_of_all_get_links = 1
-        self.link_views_dict = dict()
-        self.views_pages_number = None
-        self.soup = None
-        self.response = None
-        self.internal_numbers = None
-        self.link_title_dict = dict()
+        self.links_cleaner = URL
+
         create_csv_headers()
+        self.render_name_page()
         self.get_links()
 
     def get_links(self):
         self.index_of_internal_external_links = 0
-        self.base_url = self.search_link
         self.response = requests.get(self.search_link)
         self.soup = BeautifulSoup(self.response.text, 'html.parser')
-        self.internal_numbers = -1
         self.spider()
+
 
     def spider(self):
         for link in self.soup.find_all('a', href=True):
             get_link = f"{link.get('href')}"
             get_title = f"{link.string}"
-            self.views_pages_number = 0
-            if self.searchIDX > 3:
-                break
 
             if get_link[0:5] != "https":
                 continue
 
             if get_link in self.link_title_dict:
-                self.link_views_dict[get_link] += 1  #TODO: jesli link jest juz w
-                                                     # bazie danych dodaj +1 do liczby odnośników tego linku
+                self.link_reference_count_dict[get_link] += 1
                 continue
 
+
             else:
-                #Ustawienie dla kazdego otrzymanego linku , linku nadrzędnego
-                self.internallink_link[get_link] = self.search_link
+                self.links_cleaner = get_link
+                self.render_name_page()
+
+                # Ustawienie dla kazdego otrzymanego linku , linku nadrzednego
+                self.internal_link_link_dict[get_link] = self.search_link
                 # dodanie do słownika linku i tytułu
                 self.link_title_dict[get_link] = get_title
 
-                self.link_views_dict[get_link] = 0 #??? dodaj tą wartość dla każdego linku przy zapisie do csv
+                self.link_reference_count_dict[get_link] = 1
 
                 # licznik wszystkich znalezionych linków
                 self.index_of_all_get_links += 1
 
-                #słownik : numer linku i link , dodanie do słownika
-                self.id_link[self.index_of_all_get_links] = get_link
+                # słownik : numer linku i link , dodanie do słownika
+                self.id_link_dict[self.index_of_all_get_links] = get_link
 
-                #licznik podstron
+                # licznik podstron
                 self.index_of_internal_external_links += 1
-
-
 
         self.new_url()
 
     def new_url(self):
-        self.true_false = self.link_true_false.get(self.search_link)
-        if self.true_false == True:
+
+        self.true_false = self.link_true_false_dict.get(self.search_link)
+        if self.true_false:
             # wczytanie nadrzędnego linku
-            search_link_for_add_number_external_links = self.internallink_link.get(self.search_link)
+            search_link_for_add_number_external_links = self.internal_link_link_dict.get(self.search_link)
 
             # dodanie liczy popodstron do linku aktualnie obsługiwanej podstrony
-            self.link_number_of_external_links[search_link_for_add_number_external_links] += \
+            self.link_number_of_external_links_dict[search_link_for_add_number_external_links] += \
                 self.index_of_internal_external_links
-
 
         # następny sprawdzany link +1
         self.searchIDX += 1
 
         # dodanie do słownika linku i liczby podstron odnalezionych na tej stronie
-        self.links_index_of_internal_links[self.search_link] = self.index_of_internal_external_links
+        self.links_internal_links_count_dict[self.search_link] = self.index_of_internal_external_links
 
         # następny sprawdzany link
-        self.new_url_for_spider = self.id_link.get(self.searchIDX)
-        self.render_name_page() #???????
-
-        # nadanie link'owi wyszukujacemu nowego linku
-        self.search_link = self.new_url_for_spider
+        new_url_for_spider = self.id_link_dict.get(self.searchIDX)
+        self.search_link = new_url_for_spider
         self.get_links()
+        self.counter -= 1
+        if self.counter > 0:
+            self.get_links()
+        else:
+            pass
+            # self.add_data_to_csv()
+
+
 
     def render_name_page(self):
-        link = tuple(self.search_link)
-        actually_url = tuple(self.new_url_for_spider)
-        index_of_dot_1 = link.index(".")
-        index_of_dot_1_2 = actually_url.index(".")
-        take_url = index_of_dot_1 + 1
-        take_url_2 = index_of_dot_1_2 + 1
+        link = tuple(self.links_cleaner)
+        index_of_dot = link.index(".")
+        take_url = index_of_dot + 1
         no_dot = link[take_url:]
-        no_dot_2 = actually_url[take_url_2:]
 
         if "." not in no_dot:
-            self.basic_url = no_dot
-
-        if "." not in no_dot_2:
-            self.basic_url = no_dot_2
+            self.links_cleaner = no_dot
+            self.clear_link_reference_count_dict[self.links_cleaner] += 1
 
         if "." in no_dot:
-            index_of_dot_2 = no_dot.index('.')
-            self.basic_url = no_dot[:index_of_dot_2]
-        if "." in no_dot_2:
-            index_of_dot_2_2 = no_dot_2.index('.')
-            self.actually_url = no_dot_2[:index_of_dot_2_2]
+            index_of_dot = no_dot.index('.')
+            self.links_cleaner = no_dot[:index_of_dot]
+            self.clear_link_reference_count_dict[self.links_cleaner] += 1
 
-        self.number_internal_external_links()
-
-    def number_internal_external_links(self):
-        if self.new_url_for_spider != self.basic_url:
-            self.number_of_external_links += 1
-
-        if self.search_link == self.base_url:
-            self.internal_numbers += 1
-
-        if self.search_link != self.base_url:
-            self.external_numbers += 1
-
-    def add_data_to_csv(self):
-
-        for k, v in self.links_to_csv.items():
-            print("{}: {}".format(k, v))
-            data = {
-                'link': [k],
-                'title': [v],
-                'number of internal links': [self.index_of_internal_external_links],
-                'number of external links': [self.external_numbers],
-                'number of times url was referenced by other pages': [self.number_of_external_links]
-            }
-            df = pd.DataFrame(data)
-            df.to_csv('data.csv', mode='a', index=False, header=False)
-            self.index_of_internal_external_links = 0
-        self.links_to_csv = dict()
+    # def add_data_to_csv(self):
+    #
+    #     for a, b  in self.link_title_dict:
+    #         print("{}: {}".format(a, b))
+    #         data = {
+    #             'link': [a],
+    #             'title': [b],
+    #             'number of internal links': [self.c],
+    #             'number of external links': [self.d],
+    #             'reference count': [self.?????]
+    #         }
+    #         df = pd.DataFrame(data)
+    #         df.to_csv('data.csv', mode='a', index=False, header=False)
 
 
-start = WebCrawler("https://www.youtube.com/")
+
+
+start = WebCrawler("https://www.youtube.com/", 20)
 
 #TODO:Przepraszam za niekompletną funkcję zliczania
 # podstron i podstron podstron ale przez te nocne kodowanie przysypiam na fotelu ,
 # za mało czasu miałem na napisanie tego "Simple web crawler" z podkreśleniem na Simple :)
 # 4 dni walczyłem z szukaniem normal.mod na partycjach zeby zainstalować Ubuntu na tym dinozaurze (laptopie)
 # Cud że wogóle pyCharm na tym odpalił więc utknięcie w pętli podczas testowania programu kończyło się :black screen,
-# Coś w stylu : kiedy junior programuje ,google, stackoverflow, copy , paste , error : black screen , reset system , polubiłem już te memy :)
+# Coś w stylu : kiedy junior programuje ,google, stackoverflow, copy , paste , error : black screen , reset system ,
+# polubiłem już te memy :)
 # jeśli jakimś cudem dostanę się na ten staż to chyba tylko dlatego że ktoś doceni te moje wypociny i to że starałem
 # się jak tylko umiałem , niestety za mało czasu miałem żeby doprowadzić ten kod do 100% sprawności
 # Ps. Proszę o sprawdzenie commitów
