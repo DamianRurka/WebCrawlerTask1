ddc25373df4056e9cf9f9682a734d857081aa73f
Damian <gladek123x@gmail.com>
2022-10-15 17:35:22 +0200

    BUG FIX! 1/2

--- Newcrawler.py
+++ Newcrawler.py
@@ -7,9 +7,9 @@ import pandas as pd
 def create_csv_headers():
     headers = ["link",
                "title",
-               "number of internal links",
-               "number of external links",
-               "reference count"]
+               "internal_links",
+               "external_links",
+               "reference_count"]
     with open("data.csv", "w") as file:
         create = csv.writer(file)
         create.writerow(headers)
@@ -18,16 +18,20 @@ def create_csv_headers():
 class WebCrawler:
 
     def __init__(self, URL, count_of_results):
-        self.links_internal_links_count_dict = dict()        # słownik link:liczba podstron
-        self.link_title_dict = dict()                        # słownik link:tytuł
-        self.id_link_dict = dict()                           # słownik numer strony : tytuł
+        self.ALL_DATA_TO_CSV = {URL: {'title': "home page", 'internal_links': 0,
+                                'external_links': 0, 'reference_count': 0}}
+        self.soup = None
+        self.response = None
+        self.links_internal_links_count_dict = dict()  # słownik link:liczba podstron
+        self.link_title_dict = dict()  # słownik link:tytuł
+        self.id_link_dict = dict()  # słownik numer strony : tytuł
         self.id_link_dict[1] = URL
-        self.link_true_false_dict = dict()                   # słownik link: true/false
+        self.link_true_false_dict = dict()  # słownik link: true/false
         self.link_true_false_dict[URL] = False
-        self.internal_link_link_dict = dict()                # słownik : podstrona : link
-        self.link_number_of_external_links_dict = dict()     # słownik link : liczba podstron
-        self.clear_link_reference_count_dict = dict()        # słownik czysty link : liczba odnośników
-        self.link_reference_count_dict = dict()              # słownik link : liczba odnośników
+        self.internal_link_link_dict = dict()  # słownik : podstrona : link
+        self.link_number_of_external_links_dict = dict()  # słownik link : liczba podstron
+        self.clear_link_reference_count_dict = dict()  # słownik czysty link : liczba odnośników
+        self.link_reference_count_dict = dict()  # słownik link : liczba odnośników
         self.index_of_internal_external_links = 0
         self.search_link = URL
         self.counter = count_of_results
@@ -46,7 +50,6 @@ class WebCrawler:
         self.soup = BeautifulSoup(self.response.text, 'html.parser')
         self.spider()
 
-
     def spider(self):
         for link in self.soup.find_all('a', href=True):
             get_link = f"{link.get('href')}"
@@ -56,20 +59,20 @@ class WebCrawler:
                 continue
 
             if get_link in self.link_title_dict:
-                self.link_reference_count_dict[get_link] += 1
+                self.ALL_DATA_TO_CSV[get_link]['reference_count'] += 1
                 continue
 
-
             else:
+                self.link_true_false_dict[get_link] = True
                 self.links_cleaner = get_link
                 self.render_name_page()
 
                 # Ustawienie dla kazdego otrzymanego linku , linku nadrzednego
                 self.internal_link_link_dict[get_link] = self.search_link
                 # dodanie do słownika linku i tytułu
-                self.link_title_dict[get_link] = get_title
+                self.ALL_DATA_TO_CSV[get_link]['title'] = get_title
 
-                self.link_reference_count_dict[get_link] = 1
+                self.ALL_DATA_TO_CSV[get_link]['reference_count'] += 1
 
                 # licznik wszystkich znalezionych linków
                 self.index_of_all_get_links += 1
@@ -90,14 +93,14 @@ class WebCrawler:
             search_link_for_add_number_external_links = self.internal_link_link_dict.get(self.search_link)
 
             # dodanie liczy popodstron do linku aktualnie obsługiwanej podstrony
-            self.link_number_of_external_links_dict[search_link_for_add_number_external_links] += \
+            self.ALL_DATA_TO_CSV[search_link_for_add_number_external_links]['external_links'] += \
                 self.index_of_internal_external_links
 
         # następny sprawdzany link +1
         self.searchIDX += 1
 
         # dodanie do słownika linku i liczby podstron odnalezionych na tej stronie
-        self.links_internal_links_count_dict[self.search_link] = self.index_of_internal_external_links
+        self.links_internal_links_count_dict[self.search_link]['internal_links'] = self.index_of_internal_external_links
 
         # następny sprawdzany link
         new_url_for_spider = self.id_link_dict.get(self.searchIDX)
@@ -107,9 +110,7 @@ class WebCrawler:
         if self.counter > 0:
             self.get_links()
         else:
-             self.add_data_to_csv()
-
-
+            self.add_data_to_csv()
 
     def render_name_page(self):
         link = list(self.links_cleaner)
@@ -119,30 +120,25 @@ class WebCrawler:
         no_dot = from_end[take_url:]
         from_end = no_dot[::-1]
         joined = ''.join(from_end)
-        self.clear_link_reference_count_dict[joined] += 1
+        self.ALL_DATA_TO_CSV[joined]['reference_count'] += 1
 
     def add_data_to_csv(self):
-        self.clear_link_reference_count_dict |= self.link_reference_count_dict
 
-
-        for a, b  in self.link_title_dict.items():
-            print("{}: {}".format(a, b))
+        for a, b in self.link_title_dict.items():
             data = {
-                'link': [a],
+                'link': [],
                 'title': [b],
-                'number of internal links': [self.c],
-                'number of external links': [self.d],
-                'reference count': [self.z]
+                'number of internal links': [],
+                'number of external links': [],
+                'reference count': []
             }
             df = pd.DataFrame(data)
             df.to_csv('data.csv', mode='a', index=False, header=False)
 
 
+start = WebCrawler("https://www.youtube.com/", 5)
 
-
-start = WebCrawler("https://www.youtube.com/", 20)
-
-#TODO:Przepraszam za niekompletną funkcję zliczania
+# TODO:Przepraszam za niekompletną funkcję zliczania
 # podstron i podstron podstron ale przez te nocne kodowanie przysypiam na fotelu ,
 # za mało czasu miałem na napisanie tego "Simple web crawler" z podkreśleniem na Simple :)
 # 4 dni walczyłem z szukaniem normal.mod na partycjach zeby zainstalować Ubuntu na tym dinozaurze (laptopie)
