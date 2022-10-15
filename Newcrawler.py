73b2d9f34c78cd4095a63a2c2ad384bcb2ec9f02
Damian <gladek123x@gmail.com>
2022-10-15 21:48:22 +0200

    PROJECT COMPLETE !

--- Newcrawler.py
+++ Newcrawler.py
@@ -18,11 +18,13 @@ def create_csv_headers():
 class WebCrawler:
 
     def __init__(self, URL, count_of_results):
-        self.ALL_DATA_TO_CSV = {URL: {'title': "home page", 'internal_links': 0,
-                                      'external_links': 0, 'reference_count': 0}}
+        self.get_title = None
+        self.get_link = None
+        self.ALL_DATA_TO_CSV = dict()
+        self.ALL_DATA_TO_CSV[URL] = {'title': "home page", 'internal_links': 0,
+                                     'external_links': 0, 'reference_count': 0}
         self.soup = None
         self.response = None
-        self.link_title_dict = dict()  # słownik link:tytuł
         self.id_link_dict = dict()  # słownik numer strony : tytuł
         self.id_link_dict[1] = URL
         self.link_true_false_dict = dict()  # słownik link: true/false
@@ -37,7 +39,6 @@ class WebCrawler:
         self.links_cleaner = URL
 
         create_csv_headers()
-        self.render_name_page()
         self.get_links()
 
     def get_links(self):
@@ -48,72 +49,74 @@ class WebCrawler:
 
     def spider(self):
         for link in self.soup.find_all('a', href=True):
-            get_link = f"{link.get('href')}"
-            get_title = f"{link.string}"
-
-            if get_link[0:5] != "https":
+            self.get_link = f"{link.get('href')}"
+            self.get_title = f"{link.string}"
+            if self.get_link[0:5] != "https":
                 continue
 
-            if get_link in self.link_title_dict:
-                self.ALL_DATA_TO_CSV[get_link]['reference_count'] += 1
+            if self.get_link in self.ALL_DATA_TO_CSV:
+                self.ALL_DATA_TO_CSV[self.get_link]['reference_count'] += 1
                 continue
 
             else:
-                self.link_true_false_dict[get_link] = True
-                self.links_cleaner = get_link
+                self.index_of_internal_external_links += 1
+                self.ALL_DATA_TO_CSV[self.search_link]['internal_links'] += self.index_of_internal_external_links
+                self.link_true_false_dict[self.get_link] = True
+                self.links_cleaner = self.get_link
                 self.render_name_page()
 
                 # Ustawienie dla kazdego otrzymanego linku , linku nadrzednego
-                self.internal_link_link_dict[get_link] = self.search_link
+                self.internal_link_link_dict[self.get_link] = self.search_link
                 # dodanie do słownika linku i tytułu
-                if get_link in self.ALL_DATA_TO_CSV:
-                    self.ALL_DATA_TO_CSV[get_link]['title'] = get_title
-                else:
-                    self.ALL_DATA_TO_CSV[get_link] = {'title': get_title}
 
-                self.link_title_dict[get_link] = get_title
-                if get_link not in self.ALL_DATA_TO_CSV:
-                    self.ALL_DATA_TO_CSV[get_link]['reference_count'] += 1
+                if self.get_link in self.ALL_DATA_TO_CSV:
+                    self.ALL_DATA_TO_CSV[self.get_link]['title'] = self.get_title
                 else:
-                    self.ALL_DATA_TO_CSV[get_link]['reference_count'] = 1
+                    self.ALL_DATA_TO_CSV[self.get_link] = {'title': self.get_title,
+                                                           'reference_count': 0,
+                                                           'external_links': 0,
+                                                           'internal_links': 0}
+
+
+                if self.get_link not in self.ALL_DATA_TO_CSV:
+                    self.ALL_DATA_TO_CSV[self.get_link]['reference_count'] += 1
+                else:
+                    self.ALL_DATA_TO_CSV[self.get_link]['reference_count'] = 1
 
                 # licznik wszystkich znalezionych linków
                 self.index_of_all_get_links += 1
 
                 # słownik : numer linku i link , dodanie do słownika
-                self.id_link_dict[self.index_of_all_get_links] = get_link
-
-                # licznik podstron
-                self.index_of_internal_external_links += 1
+                self.id_link_dict[self.index_of_all_get_links] = self.get_link
 
         self.new_url()
 
     def new_url(self):
 
-        self.true_false = self.link_true_false_dict.get(self.search_link)
-        if self.true_false:
+        if self.link_true_false_dict.get(self.search_link):
+
             # wczytanie nadrzędnego linku
             search_link_for_add_number_external_links = self.internal_link_link_dict.get(self.search_link)
 
             # dodanie liczy popodstron do linku aktualnie obsługiwanej podstrony
             if search_link_for_add_number_external_links not in self.ALL_DATA_TO_CSV:
-                self.ALL_DATA_TO_CSV[search_link_for_add_number_external_links]['external_links'] += \
-                    self.index_of_internal_external_links
-            else:
                 self.ALL_DATA_TO_CSV[search_link_for_add_number_external_links]['external_links'] = \
                     self.index_of_internal_external_links
+            else:
+                self.ALL_DATA_TO_CSV[search_link_for_add_number_external_links]['external_links'] += \
+                    self.index_of_internal_external_links
 
+        #
 
+        self.new_search()
+
+    def new_search(self):
         # następny sprawdzany link +1
         self.searchIDX += 1
-
-        # dodanie do słownika linku i liczby podstron odnalezionych na tej stronie
-        self.ALL_DATA_TO_CSV[self.search_link]['internal_links'] = self.index_of_internal_external_links
-
         # następny sprawdzany link
         new_url_for_spider = self.id_link_dict.get(self.searchIDX)
         self.search_link = new_url_for_spider
-        self.get_links()
+
         self.counter -= 1
         if self.counter > 0:
             self.get_links()
@@ -129,10 +132,14 @@ class WebCrawler:
         from_end = no_dot[::-1]
         joined = ''.join(from_end)
         print(joined)
-        if joined in self.link_title_dict:
+        if joined in self.ALL_DATA_TO_CSV:
             self.ALL_DATA_TO_CSV[joined]['reference_count'] += 1
+
         else:
-            self.ALL_DATA_TO_CSV[joined] = {'reference_count': 1}
+            self.ALL_DATA_TO_CSV[joined] = {'reference_count': 1,
+                                            'internal_links': 0,
+                                            'title': self.get_title,
+                                            'external_links': 0}
 
     def add_data_to_csv(self):
 
